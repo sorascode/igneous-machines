083dfa3db60c0835db4c3773239a7af3b564fc94
David Herdlicka <david.herdlicka@gmail.com>
2023-06-14 21:17:46 +0200

    feat: igneous placer wip

--- src/main/java/com/herdlicka/igneousmachines/ExampleMod.java
+++ src/main/java/com/herdlicka/igneousmachines/ExampleMod.java
@@ -2,21 +2,44 @@ package com.herdlicka.igneousmachines;
 
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
+import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.util.Identifier;
 
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("igneous-machines");
+    // This logger is used to write text to the console and the log file.
+    // It is considered best practice to use your mod id as the logger's name.
+    // That way, it's clear which mod wrote info, warnings, and errors.
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    public static final Block IGNEOUS_PLACER_BLOCK;
+    public static final BlockItem IGNEOUS_PLACER_BLOCK_ITEM;
+    public static final BlockEntityType<IgneousPlacerBlockEntity> IGNEOUS_PLACER_BLOCK_ENTITY;
+    public static final ScreenHandlerType<IgneousPlacerScreenHandler> IGNEOUS_PLACER_SCREEN_HANDLER;
 
-		LOGGER.info("Hello Fabric world!");
-	}
+    public static final String MOD_ID = "igneous-machines";
+    // a public identifier for multiple parts of our bigger chest
+    public static final Identifier IGNEOUS_PLACER = new Identifier(MOD_ID, "igneous_placer");
+
+    static {
+        IGNEOUS_PLACER_BLOCK = Registry.register(Registries.BLOCK, IGNEOUS_PLACER, new IgneousPlacerBlock(FabricBlockSettings.copyOf(Blocks.DISPENSER)));
+        IGNEOUS_PLACER_BLOCK_ITEM = Registry.register(Registries.ITEM, IGNEOUS_PLACER, new BlockItem(IGNEOUS_PLACER_BLOCK, new Item.Settings()));
+
+        //The parameter of build at the very end is always null, do not worry about it
+        IGNEOUS_PLACER_BLOCK_ENTITY = Registry.register(Registries.BLOCK_ENTITY_TYPE, IGNEOUS_PLACER, FabricBlockEntityTypeBuilder.create(IgneousPlacerBlockEntity::new, IGNEOUS_PLACER_BLOCK).build(null));
+        IGNEOUS_PLACER_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(IGNEOUS_PLACER, IgneousPlacerScreenHandler::new);
+    }
+
+    @Override
+    public void onInitialize() {
+
+    }
 }
---
+++ src/main/java/com/herdlicka/igneousmachines/IgneousPlacerBlock.java
@@ -0,0 +1,72 @@
+package com.herdlicka.igneousmachines;
+
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ItemScatterer;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class IgneousPlacerBlock extends BlockWithEntity {
+    public IgneousPlacerBlock(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new IgneousPlacerBlockEntity(pos, state);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        //With inheriting from BlockWithEntity this defaults to INVISIBLE, so we need to change that!
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            //This will call the createScreenHandlerFactory method from BlockWithEntity, which will return our blockEntity cast to
+            //a namedScreenHandlerFactory. If your block class does not extend BlockWithEntity, it needs to implement createScreenHandlerFactory.
+            NamedScreenHandlerFactory screenHandlerFactory = state.createScreenHandlerFactory(world, pos);
+
+            if (screenHandlerFactory != null) {
+                //With this call the server will request the client to open the appropriate Screenhandler
+                player.openHandledScreen(screenHandlerFactory);
+            }
+        }
+        return ActionResult.SUCCESS;
+    }
+
+
+    //This method will drop all items onto the ground when the block is broken
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        if (state.getBlock() != newState.getBlock()) {
+            BlockEntity blockEntity = world.getBlockEntity(pos);
+            if (blockEntity instanceof IgneousPlacerBlockEntity) {
+                ItemScatterer.spawn(world, pos, (IgneousPlacerBlockEntity)blockEntity);
+                // update comparators
+                world.updateComparators(pos,this);
+            }
+            super.onStateReplaced(state, world, pos, newState, moved);
+        }
+    }
+
+    @Override
+    public boolean hasComparatorOutput(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
+        return ScreenHandler.calculateComparatorOutput(world.getBlockEntity(pos));
+    }
+}
